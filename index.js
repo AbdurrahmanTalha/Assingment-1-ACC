155f2b967b1e417dae861acf3abadeb10e8fde86
AbdurrahmanTalha <abdurrahmantalha331@gmail.com>
2022-09-03 21:54:10 +0600

    finished

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const usersRoute = require("./routes/user.routes")
+require("dotenv").config()
+
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+
 
 app.use(express.json());
 app.use(cors());
